5eb3108e553716d69eeb4cc8e3ca12cabfc0dc5a
sarawaniolka <waniolka.sara@gmail.com>
2022-03-27 17:40:00 +0200

    solved

--- main.py
+++ main.py
@@ -10,10 +10,8 @@ grid = [[5,3,0,0,7,0,0,0,0],
         [7,0,0,0,2,0,0,0,6],
         [0,6,0,0,0,0,2,8,0],
         [0,0,0,0,1,9,0,0,5],
-        [0,0,0,0,0,0,0,0,0]]
+        [0,0,0,0,0,0,0,1,0]]
 
-
-print(np.matrix(grid))
 
 def check(r,c,nb):
     global grid
@@ -27,11 +25,26 @@ def check(r,c,nb):
             return False
     # cheick if the number is in the square
     # creating the 3 sections
-     x0 = (c//3)*3
-     y0 = (r//3)*3
-     for i in range(0,3):
-         for j in range(0,3):
-             if grid[y0+i][x0+j] == nb:
-                 return False
-        
+    x0 = (c//3)*3
+    y0 = (r//3)*3
+    for i in range(0,3):
+        for j in range(0,3):
+            if grid[y0+i][x0+j] == nb:
+                return False
+    return True
+
+def solve():
+    global grid
+    for r in range(0,9):
+        for c in range(0,9):
+            if grid[r][c] == 0:
+                for nb in range(0,10):
+                    if check(r,c,nb):
+                        grid[r][c] = nb
+                        solve()
+                        grid[r][c] = 0
+                return
+    print(np.matrix(grid))
+    input('More possible solutions')
     
+solve()
